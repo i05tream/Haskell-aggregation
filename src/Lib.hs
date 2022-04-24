f9944921b1078cd3c053508b40c0d1405766dc23
i05tream <i05tream255@gmail.com>
2022-04-24 21:06:08 +0900

    implement median

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,21 @@
 module Lib
 where
 
+import Data.List (sort)
+
 average :: [Int] -> Maybe Int
 average [] = Nothing
 average xs = Just $ sum xs `div` (length xs)
 
-median :: [Int] -> Maybe Int
+median :: (Integral a) => [a] -> Maybe Double
 median [] = Nothing
+median xs =
+  let len = length xs
+      xs' = sort xs
+  in
+    if odd len
+      then Just . fromIntegral $ xs !! (len `div` 2)
+      else Just $
+        let median1 = xs !! (len `div` 2 - 1)
+            median2 = xs !! (len `div` 2)
+        in fromIntegral (median1 + median2) / 2
